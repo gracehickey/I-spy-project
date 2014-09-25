ea9604bb78a11bfc4ffa587331cd5bcf8e5b4e90
grace.hickey <grace.hickey@MU-C3-11.mullen.pvt.k12.co.us>
2014-09-25 15:32:24 -0600

    Thursday Commit

--- ISPY/src/Game.java
+++ ISPY/src/Game.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class Game
 	{
 	private static String chosencolor;
+	static String [] chosenArray = new String [3];
 
 	public static void main(String[] args) 
 	 	{
 		 String[]colorsInTheRoomArray={"blue", "pink", "black", "yellow", "red"};	
 		 int randomWord = (int)(Math.random()*5);
+		 int randomNumber = (int)(Math.random()*3);
+		 System.out.println(randomNumber);
 		 switch(colorsInTheRoomArray[randomWord])
 			{
-				case "blue":
+				case "blue": 
+					{
+					chosenArray = Data.blue;
+					}
 				case "pink":
 				case "black":
 				case "yellow":
@@ -20,14 +26,6 @@ public class Game
 				{
 					String chosencolor = colorsInTheRoomArray[randomWord];
 				}
-			ArrayList <String> blue = new ArrayList <String>();
-			ArrayList <String> pink = new ArrayList <String>();
-			ArrayList <String> black = new ArrayList <String>();
-			ArrayList <String> yellow = new ArrayList <String>();
-			ArrayList <String> red = new ArrayList <String>();
-			System.out.println("I spy something" + chosencolor);
-			Scanner userinput = new Scanner(System.in);
-			String guess = userinput.nextLine();
 			
 			
 			
