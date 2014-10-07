80357c6bbfb28cc646424c89f8f989ecd1f54250
grace.hickey <grace.hickey@MU-C3-11.mullen.pvt.k12.co.us>
2014-10-07 15:17:25 -0600

    At school

--- ISPY/src/Game.java
+++ ISPY/src/Game.java
@@ -4,12 +4,14 @@ public class Game
 	{
 	private static String chosencolor;
 	static String [] chosenArray = new String [3];
+	static int guess;
+	static boolean userStillGuessing = true;
 
 	public static void main(String[] args) 
 	 	{
 		 String[]colorsInTheRoomArray={"blue", "pink", "black", "yellow", "red"};	
 		 int randomWord = (int)(Math.random()*5);
-		 int randomNumber = (int)(Math.random()*3);
+		 int randomNumber = (int)(Math.random()*3 + 1);
 		 System.out.println("I spy with my silicon eye something " + colorsInTheRoomArray[randomWord] + ".");
 		 System.out.println("What do you think I am thinking of?");
 		 switch(colorsInTheRoomArray[randomWord])
@@ -17,24 +19,29 @@ public class Game
 				case "blue": 
 					{
 					chosenArray = Data.blue;
+					break;
 					}
 				case "pink":
 					{
 					chosenArray = Data.pink; 
+					break;
 					}
 				case "black":
 					{
 					chosenArray = Data.black;
+					break;
 					}
 				case "yellow":
 					{
 					chosenArray = Data.yellow;	
+					break;
 					}
 				case "red":
 					{
 					chosenArray = Data.red;	
-					}
 					break;
+					}
+					
 				}
 		 		
 		 for(int i = 0; i < 3; i++)
@@ -43,16 +50,23 @@ public class Game
 			System.out.println(". " + chosenArray[i]);
 		 	}
 		
-		 boolean userStillGuessing = true;
 		 while(userStillGuessing)
 		 	{
 			 Scanner inputUser = new Scanner(System.in);
-			 
-			 
-		 	}
-		}
+			 guess = inputUser.nextInt();
+			 if(guess == randomNumber)
+				 {
+				 System.out.println("You're right!");
+				 userStillGuessing = false; 
+				 }
+		 else
+			 {
+			 System.out.println("Try again.");
+			 }
+		 }
 
 	
-	}
+	 	}
+}
 
 	
